b40e28d785b0c9d62779d7a67167b6caa67722f2
Damian Hill <dhill098@gmail.com>
2014-07-26 22:44:54 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-ajax({ url: 'http://www.thebluealliance.com/event/2014flor' }, function(data){
+ajax({ url: 'http://www.thebluealliance.com/api/v2/team/frc2383/event/2014vari/matches' }, function(data){
   var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
   simply.title(headline);
 });
